9f8e32a737a35c0eae0011a503de44a7fe8b764b
İrfan Evrens <irfanevrens@gmail.com>
2014-12-09 10:26:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,43 @@
 <?php
-	/* Hadi Bismillah */
-	# değişken
-	$q=@$_GET["q"];# q değişkenimizi aldık
-	$q=@explode("/",$q);# q değişkenimizi parçaladık
-	# / değişken
-	# sistem ana dosyaları
-	include "colak/ana.php";#fonksiyonlarımızın bulunduğu class ımızı include ettik
-	include "system/libs/colak.php";#clk class
-	include "system/libs/load.php";#yükleme class ımızı include ettik
-	include "colak/tag.php";#tag class ı
-	include "system/libs/controller.php";#yönlendirici class ımızı include ettik
-	include "system/libs/model.php";#veritabanı işlemlerini yapacağımız class ımızı include ettik
-	# / sistem ana dosyaları
-	clk::baslangic();
-	if($q[0]==""){
-		include "apps/controller/anasayfa.php";
-		$controller=new anasayfa(array("anasayfa"));
-	}#eğer q değişkeni yoksa standart yönlendiricimizi çalıştırdık
-	else{
-		if(is_file("apps/controller/".$q[0].".php")){
-			include "apps/controller/".$q[0].".php";
-			$controller=new $q[0]($q);
-		}#eğer yönlendirici mevcut ise çalıştırdık
-		else{
-			include "apps/controller/hata.php";
-			$controller=new hata();
-		}#eğer yönlendirici mevcut değilse standart hata sayfamızı çalıştırdık
-	}
-	clk::cacheOnlem(md5(implode(".",$q)).".php");
-	/* Bu MVC Framework ün kodlaması tamamen Mustafa Çolakoğluna aittir. */
-	/* colakMVC v1.0 */
-	/* İletişim:musto_220@windowslive.com */
-?>
+	
+/* Hadi Bismillah */
+
+# değişken
+$q=@$_GET["q"];# q değişkenimizi aldık
+$q=@explode("/",$q);# q değişkenimizi parçaladık
+# / değişken
+
+# sistem ana dosyaları
+include "colak/ana.php";#fonksiyonlarımızın bulunduğu class ımızı include ettik
+include "system/libs/colak.php";#clk class
+include "system/libs/load.php";#yükleme class ımızı include ettik
+include "colak/tag.php";#tag class ı
+include "system/libs/controller.php";#yönlendirici class ımızı include ettik
+include "system/libs/model.php";#veritabanı işlemlerini yapacağımız class ımızı include ettik
+# / sistem ana dosyaları
+
+clk::baslangic();
+
+if ($q[0] == ""){
+
+	include "apps/controller/anasayfa.php";
+	$controller=new anasayfa(array("anasayfa"));
+} else { #eğer q değişkeni yoksa standart yönlendiricimizi çalıştırdık
+
+	if (is_file("apps/controller/".$q[0].".php")) {
+		
+		#eğer yönlendirici mevcut ise çalıştırdık
+		include "apps/controller/".$q[0].".php";
+		$controller=new $q[0]($q);
+	} else { 
+	
+		#eğer yönlendirici mevcut değilse standart hata sayfamızı çalıştırdık
+	
+		include "apps/controller/hata.php";
+		$controller=new hata();
+	} 
+}
+clk::cacheOnlem(md5(implode(".",$q)).".php");
+/* Bu MVC Framework ün kodlaması tamamen Mustafa Çolakoğluna aittir. */
+/* colakMVC v1.0 */
+/* İletişim:musto_220@windowslive.com */
